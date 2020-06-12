114d66c5713cae2d9ce8ec6aea8b0d334a75d2c6
Douglas Shibata <douglas.shibata11@gmail.com>
2020-06-12 15:50:50 +0000

    Finalizando o jogo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MemoryGame
 Jogo Da memória desenvolvido em JS
+Com o auxílio do vídeo https://www.youtube.com/watch?v=lhNdUVh3qCc
--- app.js
+++ app.js
@@ -1,28 +1,186 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const cardArray=[
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    }
+  ]
+
+  cardArray.sort(() => 0.5 - Math.random())
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
+  var cardsChosen = []
+  var cardsChosenId = []
+  const cardsWon = []
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement('img')
+      card.setAttribute('src', 'images/blank.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    
+    if(optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('You have clicked the same image!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert('You found a match')
+      cards[optionOneId].setAttribute('src', 'images/white.png')
+      cards[optionTwoId].setAttribute('src', 'images/white.png')
+      cards[optionOneId].removeEventListener('click', flipCard)
+      cards[optionTwoId].removeEventListener('click', flipCard)
+      cardsWon.push(cardsChosen)
+    } else {
+      cards[optionOneId].setAttribute('src', 'images/blank.png')
+      cards[optionTwoId].setAttribute('src', 'images/blank.png')
+      alert('Sorry, try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if  (cardsWon.length === cardArray.length/2) {
+      resultDisplay.textContent = 'Congratulations! You found them all!'
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length ===2) {
+      setTimeout(checkForMatch, 500)
+    }
+  }
+
+  createBoard()
+})
+/* document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
         {
-            name:'fries',
-            img:'',
+            name: 'fries',
+            img: 'images/fries.png',
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png',
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png',
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png',
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png',
         },
     ]
     const grid = document.querySelector('.grid');
-
+    var cardsEscolhidas = [];
+    var cardsEscolhidasId =[];
+    var cardsWon = [];
     //criando tabuleiro 
-    function createBoard(){
-        for (let index = 0; index < array.length; index++) {
-             const card = document.createElement('img');
-             card.setAttribute('src','images/blank.png');
-             card.setAttribute('data-id',i);
-             card.addEventListener('click',flipCard);
-             grid.appendChild(card)
-            
+    function createBoard() {
+        for (let index = 0; index < cardArray.length; index++) {
+            const card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', index);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card)
+
         }
     }
-
+    //Verificando iguais
+    function checkForMatch(){
+        let cards = document.querySelectorAll('img');
+        const optionOneId = cardsEscolhidasId[0];
+        const optionTwoId = cardsEscolhidasId[1];
+        if(cardsEscolhidas[0]==cardsEscolhidas[1]){
+            alert('Não encontrado');
+            cards[optionOneId].setAttribute('src','images/white.png');
+            cards[optionTwoId].setAttribute('src','images/white.png');
+            cardsWon.pushc(cardsEscolhidas)
+        }else{
+            cards[optionOneId].setAttribute('src','images/blank.png');
+            cards[optionTwoId].setAttribute('src','images/blank.png');
+            alert('tente novamente')
+        }
+        cardsEscolhidas=[];
+        cardsEscolhidasId[];
+        
+    }
     //virando a carta
-    function flipCard(){
-        let cardId = this,
+    function flipCard() {
+        let cardId = this.getAttribute('data-id');
+        cardsEscolhidas.push(cardArray[cardId].name);
+        cardsEscolhidasId.push(cardId);
+        this.setAttribute('src',cardArray[cardId].img);
+        if(cardsEscolhidas.length==2){
+            setTimeout(checkForMatch,500)
+        }
 
     }
     createBoard();
-})
+}) */
